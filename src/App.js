18c34b3b97b4da6451aa5e3ff7597d765baa001d
kavya rajput <131941640+rajputrajput30@users.noreply.github.com>
2023-09-25 00:07:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import './App.css';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Header_aboutus from './components_ABoutUs/Header_aboutus/Header_aboutus';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Home from './pages/Home/Home';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import TopView from "./components/topView/TopView";
+import Programs from "./components/our-programs_div/Programs";
+import Footer from "./components/footer/Footer";
 
-
-// internship_assingment
 function App() {
+  const program_headings = [
+    {
+      main_head:"Full Time Job Roles",
+      card_info:[{head:"Blockchain Developer"},{head:"Phyton Developer"},{head:"MERN stack Developer"}]
+    }
+    ,
+    {
+      main_head:"Summer Internships",
+      card_info:[{head:"fullstack Developer"},{head:"MEAN Developer"},{head:"JAVA Developer"}]
+    }
+    ,
+    {
+      main_head:"Offline Internships",
+      card_info:[{head:"Data Analyst"},{head:"HR Requriter"},{head:"C++ Developer"}]
+    }
+    ,
+    {
+      main_head: "Online Internships",
+      card_info:[{head:"Phyton Certification"},{head:"Java Certification"},{head:"UI/UX Developer"}]
+    }
+    
+  ];
+  
+
   return (
-  <>
-    <Navbar/>
-    <AboutUs/>
-    <Footer/>
-  </>
+    <div className="App">
+      <Navbar />
+      <TopView />
+      <Programs heading={program_headings[0]} />
+      <Programs heading={program_headings[1]} />
+      <Programs heading={program_headings[2]} />
+      <Programs heading={program_headings[3]} />
+
+      <Footer />
+    </div>
   );
 }
 
